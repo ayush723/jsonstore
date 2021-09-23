cdfc7b67525269002a700f55147fb9e6e05665c6
Ayush Paudel <ayushpaudel20@gmail.com>
2021-09-23 08:03:56 +0545

    made connection to psql

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/ayush723/jsonstore
 go 1.16
 
 require (
-	github.com/lib/pq v1.10.3
+	github.com/gorilla/mux v1.8.0
+	github.com/lib/pq v1.10.3 // indirect
 	gorm.io/driver/postgres v1.1.1
 	gorm.io/gorm v1.21.15
 )
--- models/models.go
+++ models/models.go
@@ -1,36 +1,44 @@
 package models
 
 import (
+	"database/sql"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-type User struct{
+type User struct {
 	gorm.Model
 	Orders []Order
-	Data string `sql:"type:JSONB NOT NULL DEFAULT '{}'::JSONB" json:"-"`
+	Data   string `sql:"type:JSONB NOT NULL DEFAULT '{}'::JSONB" json:"-"`
 }
 
-type Order struct{
+type Order struct {
 	gorm.Model
 	User User
 	Data string `sql:"type:JSONB NOT NULL DEFAULT '{}'::JSONB"`
 }
 
 //GORM creates tables with plural names.
-func (User) TableName() string{
+func (User) TableName() string {
 	return "user"
-} 
+}
 
-func (Order) TableName() string{
+func (Order) TableName() string {
 	return "order"
 }
 
-func InitDB() (*gorm.DB, error){
+func InitDB() (*gorm.DB, error) {
 	var err error
-	dsn := "host=localhost user=ayush password=envy dbname=jsonstore port=5432 sslmode=disable"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	dsn := "postgres://ayush:envy@tcp(127.0.0.1:5432)/jsonstore"
+	sqlDB, err := sql.Open("postgres",dsn)
 	if err != nil{
+		panic(err)
+	}
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		Conn: sqlDB,
+	}), &gorm.Config{})
+	if err != nil {
 		return nil, err
 	} else {
 		//the below automigrate is equivalent to this
@@ -41,7 +49,7 @@ func InitDB() (*gorm.DB, error){
 		// 	db.Migrator().CreateTable(&Order{})
 		// }
 
-		db.AutoMigrate(&User{},&Order{})
+		db.AutoMigrate(&User{}, &Order{})
 		return db, nil
 	}
 
